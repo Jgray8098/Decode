274688b9f5dcb9f817b419010e3cd87cf57728e9
Jeremy Gray <whextra330@yahoo.com>
2026-06-04 06:43:41 -0500

    Pedro pathing tuning constants updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -14,7 +16,14 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(11.113);
+            .mass(11.113)
+            .forwardZeroPowerAcceleration(-29.34215361)
+            .lateralZeroPowerAcceleration(-74.0827393)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.03, 0, 0.001, 0.035))
+            .headingPIDFCoefficients(new PIDFCoefficients(0.45, 0, 0.002, 0.03))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.1, 0, 0.002, 0.6, 0.025))
+            .centripetalScaling(0.0005);
+
 
     public static MecanumConstants driveConstants = new MecanumConstants()
         .maxPower(1)
@@ -30,7 +39,7 @@ public class Constants {
         .yVelocity(68.06904931);
 
     
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1.4, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
